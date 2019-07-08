5677f31432c686b490f548d7ef499c3911442cc6
Spencer Corwin <spencercorwin@icloud.com>
2019-07-07 23:51:15 -0700

    Update populariz link

--- src/projects.js
+++ src/projects.js
@@ -6,7 +6,7 @@ const Projects = () => {
         <div id="projects">
             <h3 className="project-title">Personal Projects</h3>
             <ul className="list-style">
-                <li><a href="https://popularity-contest.herokuapp.com/">Popularity Contest App</a></li>
+                <li><a href="https://populariz-frontend.herokuapp.com/">Popularity Contest App</a></li>
                 <li><a href="https://twitter.com/dailygoldquotes">Daily Gold and Silver Quotes Twitter Bot</a></li>
                 <li><a href="https://github.com/spencercorwin/corwinism-vscode-theme">Corwinism VS Code Theme</a></li>
                 <li><a href="https://github.com/spencercorwin/corwinism-zsh-theme">Corwinism ZSH Theme</a></li>
